0bc991ab4bc473077448d75bbe13b21aeebc30e6
KB <katrinkajb@gmail.com>
2021-03-09 10:49:36 -0800

    first working test!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,6 +18,7 @@ describe('app routes', () => {
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
+          name: 'John',
           email: 'jon@user.com',
           password: '1234'
         });
@@ -31,35 +32,199 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    // GET edible search
+    test('returns the first result from a list of edible plants ', async() => {
 
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+      const expectation = {
+        'id': 176845,
+        'common_name': 'Garden sorrel',
+        'slug': 'rumex-acetosa',
+        'scientific_name': 'Rumex acetosa',
+        'year': 1753,
+        'bibliography': 'Sp. pl. 1:337.  1753, nom. cons.',
+        'author': 'L.',
+        'status': 'accepted',
+        'rank': 'species',
+        'family_common_name': 'Buckwheat family',
+        'genus_id': 2722,
+        'image_url': 'https://bs.plantnet.org/image/o/780b9f3c63318588b8874d608c2d4900fc2adce3',
+        'synonyms': [
+          'Acetosa pratensis subsp. fontanopaludosa',
+      
+          'Acetosa officinalis',
+      
+          'Rumex planellae',
+      
+          'Lapathum pratense',
+      
+          'Rumex fontanopaludosus',
+      
+          'Rumex triangularis',
+      
+          'Acetosa angustata',
+      
+          'Acetosa amplexicaulis',
+      
+          'Rumex oxyotus',
+      
+          'Acetosa hastulata',
+      
+          'Rumex oblongus',
+      
+          'Acetosa bidentula',
+      
+          'Acetosa olitoria',
+      
+          'Rumex acetosa subsp. biformis',
+      
+          'Acetosa pratensis subsp. biformis',
+      
+          'Rumex angustatus',
+      
+          'Rumex micranthus',
+      
+          'Acetosa subalpina',
+      
+          'Acetosa hastifolia',
+      
+          'Rumex pseudoacetosa',
+      
+          'Rumex commersonii',
+          'Acetosa fontanopaludosa',
+          'Rumex olitoria',
+          'Acetosa agrestis',
+          'Rumex agrestis',
+          'Rumex acuminatus',
+          'Rumex sagittifolius',
+          'Rumex biformis',
+          'Rumex bidentula',
+          'Acetosa pratensis',
+          'Rumex bulbosus',
+          'Lapathum acetosa'
+      
+        ],
+      
+        'genus': 'Rumex',
+      
+        'family': 'Polygonaceae',
+      
+        'links': {
+      
+          'self': '/api/v1/species/rumex-acetosa',
+      
+          'plant': '/api/v1/plants/rumex-acetosa',
+      
+          'genus': '/api/v1/genus/rumex'
+      
         }
-      ];
+      
+      };
 
-      const data = await fakeRequest(app)
-        .get('/animals')
+      const response = await fakeRequest(app)
+        .get('/api/edible_search')
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
-      expect(data.body).toEqual(expectation);
+      expect(response.body.data[0]).toEqual(expectation);
     });
   });
+
+  // something else
+  // test('returns the first result from a list of edible plants ', async() => {
+
+  //   const expectation = {
+  //     'id': 176845,
+  //     'common_name': 'Garden sorrel',
+  //     'slug': 'rumex-acetosa',
+  //     'scientific_name': 'Rumex acetosa',
+  //     'year': 1753,
+  //     'bibliography': 'Sp. pl. 1:337.  1753, nom. cons.',
+  //     'author': 'L.',
+  //     'status': 'accepted',
+  //     'rank': 'species',
+  //     'family_common_name': 'Buckwheat family',
+  //     'genus_id': 2722,
+  //     'image_url': 'https://bs.plantnet.org/image/o/780b9f3c63318588b8874d608c2d4900fc2adce3',
+  //     'synonyms': [
+  //       'Acetosa pratensis subsp. fontanopaludosa',
+    
+  //       'Acetosa officinalis',
+    
+  //       'Rumex planellae',
+    
+  //       'Lapathum pratense',
+    
+  //       'Rumex fontanopaludosus',
+    
+  //       'Rumex triangularis',
+    
+  //       'Acetosa angustata',
+    
+  //       'Acetosa amplexicaulis',
+    
+  //       'Rumex oxyotus',
+    
+  //       'Acetosa hastulata',
+    
+  //       'Rumex oblongus',
+    
+  //       'Acetosa bidentula',
+    
+  //       'Acetosa olitoria',
+    
+  //       'Rumex acetosa subsp. biformis',
+    
+  //       'Acetosa pratensis subsp. biformis',
+    
+  //       'Rumex angustatus',
+    
+  //       'Rumex micranthus',
+    
+  //       'Acetosa subalpina',
+    
+  //       'Acetosa hastifolia',
+    
+  //       'Rumex pseudoacetosa',
+    
+  //       'Rumex commersonii',
+  //       'Acetosa fontanopaludosa',
+  //       'Rumex olitoria',
+  //       'Acetosa agrestis',
+  //       'Rumex agrestis',
+  //       'Rumex acuminatus',
+  //       'Rumex sagittifolius',
+  //       'Rumex biformis',
+  //       'Rumex bidentula',
+  //       'Acetosa pratensis',
+  //       'Rumex bulbosus',
+  //       'Lapathum acetosa'
+    
+  //     ],
+    
+  //     'genus': 'Rumex',
+    
+  //     'family': 'Polygonaceae',
+    
+  //     'links': {
+    
+  //       'self': '/api/v1/species/rumex-acetosa',
+    
+  //       'plant': '/api/v1/plants/rumex-acetosa',
+    
+  //       'genus': '/api/v1/genus/rumex'
+    
+  //     }
+    
+  //   };
+
+  //   const response = await fakeRequest(app)
+  //     .get('/api/edible_search')
+  //     .set('Authorization', token)
+  //     .expect('Content-Type', /json/)
+  //     .expect(200);
+
+  //   expect(response.body.data[0]).toEqual(expectation);
+  // });
 });
+
