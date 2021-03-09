7f58527dd5dba0cf3c1cad137f1450c3c34c6e70
Nicole Martin <n.martinpdx@gmail.com>
2021-03-09 14:49:18 -0800

    add many tests working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,86 +9,84 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
-    beforeAll(async done => {
+
+    beforeAll(async (done) => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          name: 'John',
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
+
+      const signInData = await fakeRequest(app).post('/auth/signup').send({
+        name: 'John',
+        email: 'jon@user.com',
+        password: '1234',
+      });
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
-    afterAll(done => {
+
+    afterAll((done) => {
       return client.end(done);
     });
 
     // GET edible search
-    test('returns the first result from a list of edible plants ', async() => {
-
+    test('returns the first result from a list of edible plants ', async () => {
       const expectation = {
-        'id': 176845,
-        'common_name': 'Garden sorrel',
-        'slug': 'rumex-acetosa',
-        'scientific_name': 'Rumex acetosa',
-        'year': 1753,
-        'bibliography': 'Sp. pl. 1:337.  1753, nom. cons.',
-        'author': 'L.',
-        'status': 'accepted',
-        'rank': 'species',
-        'family_common_name': 'Buckwheat family',
-        'genus_id': 2722,
-        'image_url': 'https://bs.plantnet.org/image/o/780b9f3c63318588b8874d608c2d4900fc2adce3',
-        'synonyms': [
+        id: 176845,
+        common_name: 'Garden sorrel',
+        slug: 'rumex-acetosa',
+        scientific_name: 'Rumex acetosa',
+        year: 1753,
+        bibliography: 'Sp. pl. 1:337.  1753, nom. cons.',
+        author: 'L.',
+        status: 'accepted',
+        rank: 'species',
+        family_common_name: 'Buckwheat family',
+        genus_id: 2722,
+        image_url:
+          'https://bs.plantnet.org/image/o/780b9f3c63318588b8874d608c2d4900fc2adce3',
+        synonyms: [
           'Acetosa pratensis subsp. fontanopaludosa',
-      
+
           'Acetosa officinalis',
-      
+
           'Rumex planellae',
-      
+
           'Lapathum pratense',
-      
+
           'Rumex fontanopaludosus',
-      
+
           'Rumex triangularis',
-      
+
           'Acetosa angustata',
-      
+
           'Acetosa amplexicaulis',
-      
+
           'Rumex oxyotus',
-      
+
           'Acetosa hastulata',
-      
+
           'Rumex oblongus',
-      
+
           'Acetosa bidentula',
-      
+
           'Acetosa olitoria',
-      
+
           'Rumex acetosa subsp. biformis',
-      
+
           'Acetosa pratensis subsp. biformis',
-      
+
           'Rumex angustatus',
-      
+
           'Rumex micranthus',
-      
+
           'Acetosa subalpina',
-      
+
           'Acetosa hastifolia',
-      
+
           'Rumex pseudoacetosa',
-      
+
           'Rumex commersonii',
           'Acetosa fontanopaludosa',
           'Rumex olitoria',
@@ -100,24 +98,20 @@ describe('app routes', () => {
           'Rumex bidentula',
           'Acetosa pratensis',
           'Rumex bulbosus',
-          'Lapathum acetosa'
-      
+          'Lapathum acetosa',
         ],
-      
-        'genus': 'Rumex',
-      
-        'family': 'Polygonaceae',
-      
-        'links': {
-      
-          'self': '/api/v1/species/rumex-acetosa',
-      
-          'plant': '/api/v1/plants/rumex-acetosa',
-      
-          'genus': '/api/v1/genus/rumex'
-      
-        }
-      
+
+        genus: 'Rumex',
+
+        family: 'Polygonaceae',
+
+        links: {
+          self: '/api/v1/species/rumex-acetosa',
+
+          plant: '/api/v1/plants/rumex-acetosa',
+
+          genus: '/api/v1/genus/rumex',
+        },
       };
 
       const response = await fakeRequest(app)
@@ -130,33 +124,33 @@ describe('app routes', () => {
     });
 
     // GET filtered search
-    test('returns the first result from a list of edible plants filtered by edible part=roots, vegetable=true, and light=0,8', async() => {
-
+    test('returns the first result from a list of edible plants filtered by edible part=roots, vegetable=true, and light=0,8', async () => {
       const expectation = {
-        'id': 147680,
-        'common_name': 'Tuberous sweetpea',
-        'slug': 'lathyrus-tuberosus',
-        'scientific_name': 'Lathyrus tuberosus',
-        'year': 1753,
-        'bibliography': 'Sp. pl. 2:732.  1753',
-        'author': 'L.',
-        'status': 'accepted',
-        'rank': 'species',
-        'family_common_name': 'Pea family',
-        'genus_id': 3994,
-        'image_url': 'https://bs.plantnet.org/image/o/2a9df55f9f77e98002806afe05b78392ab017f4d',
-        'synonyms': [
+        id: 147680,
+        common_name: 'Tuberous sweetpea',
+        slug: 'lathyrus-tuberosus',
+        scientific_name: 'Lathyrus tuberosus',
+        year: 1753,
+        bibliography: 'Sp. pl. 2:732.  1753',
+        author: 'L.',
+        status: 'accepted',
+        rank: 'species',
+        family_common_name: 'Pea family',
+        genus_id: 3994,
+        image_url:
+          'https://bs.plantnet.org/image/o/2a9df55f9f77e98002806afe05b78392ab017f4d',
+        synonyms: [
           'Pisum tuberosum',
           'Lathyrus festivus',
-          'Lathyrus tuberculatus'
+          'Lathyrus tuberculatus',
         ],
-        'genus': 'Lathyrus',
-        'family': 'Fabaceae',
-        'links': {
-          'self': '/api/v1/species/lathyrus-tuberosus',
-          'plant': '/api/v1/plants/lathyrus-tuberosus',
-          'genus': '/api/v1/genus/lathyrus'
-        }
+        genus: 'Lathyrus',
+        family: 'Fabaceae',
+        links: {
+          self: '/api/v1/species/lathyrus-tuberosus',
+          plant: '/api/v1/plants/lathyrus-tuberosus',
+          genus: '/api/v1/genus/lathyrus',
+        },
       };
 
       const response = await fakeRequest(app)
@@ -169,22 +163,22 @@ describe('app routes', () => {
     });
 
     // GET search by name input
-    test('returns the first result from a list of edible plants filtered by search input', async() => {
-
+    test('returns the first result from a list of edible plants filtered by search input', async () => {
       const expectation = {
-        'id': 165409,
-        'common_name': 'Kidney bean',
-        'slug': 'phaseolus-vulgaris',
-        'scientific_name': 'Phaseolus vulgaris',
-        'year': 1753,
-        'bibliography': 'Sp. pl. 2:723.  1753',
-        'author': 'L.',
-        'status': 'accepted',
-        'rank': 'species',
-        'family_common_name': 'Pea family',
-        'genus_id': 3221,
-        'image_url': 'https://bs.plantnet.org/image/o/46b23b23e98319531962424fb9d88a4656610a0b',
-        'synonyms': [
+        id: 165409,
+        common_name: 'Kidney bean',
+        slug: 'phaseolus-vulgaris',
+        scientific_name: 'Phaseolus vulgaris',
+        year: 1753,
+        bibliography: 'Sp. pl. 2:723.  1753',
+        author: 'L.',
+        status: 'accepted',
+        rank: 'species',
+        family_common_name: 'Pea family',
+        genus_id: 3221,
+        image_url:
+          'https://bs.plantnet.org/image/o/46b23b23e98319531962424fb9d88a4656610a0b',
+        synonyms: [
           'Phaseolus nanus',
           'Phaseolus haematocarpus',
           'Phaseolus asparagioides',
@@ -225,15 +219,15 @@ describe('app routes', () => {
           'Phaseolus gonospermos',
           'Phaseolus sinensis',
           'Phaseolus saccharatus',
-          'Phaseolus vulgaris var. humilis'
+          'Phaseolus vulgaris var. humilis',
         ],
-        'genus': 'Phaseolus',
-        'family': 'Fabaceae',
-        'links': {
-          'self': '/api/v1/species/phaseolus-vulgaris',
-          'plant': '/api/v1/plants/phaseolus-vulgaris',
-          'genus': '/api/v1/genus/phaseolus'
-        }
+        genus: 'Phaseolus',
+        family: 'Fabaceae',
+        links: {
+          self: '/api/v1/species/phaseolus-vulgaris',
+          plant: '/api/v1/plants/phaseolus-vulgaris',
+          genus: '/api/v1/genus/phaseolus',
+        },
       };
 
       const response = await fakeRequest(app)
@@ -246,13 +240,12 @@ describe('app routes', () => {
     });
 
     // GET plant details by id
-    test('returns the details for a plant from a given id', async() => {
-
+    test('returns the details for a plant from a given id', async () => {
       const expectation = {
-        'data': {
-          'id': 116630,
-          'common_name': 'coconut palm',
-        }
+        data: {
+          id: 116630,
+          common_name: 'coconut palm',
+        },
       };
 
       const response = await fakeRequest(app)
@@ -261,22 +254,19 @@ describe('app routes', () => {
         .expect('Content-Type', /json/)
         .expect(200);
 
-      expect(response.body.data.common_name).toEqual(expectation.data.common_name);
+      expect(response.body.data.common_name).toEqual(
+        expectation.data.common_name
+      );
     });
 
     // POST to wishlist
-    test('adds a plant to the wishlist', async() => {
-
+    test('adds a plant to the wishlist', async () => {
       const newWishlistPlant = {
         main_species_id: 122263,
-        owner_id: 1
+        owner_id: 2,
       };
 
-      const dbWishlist = [
-        { ...newWishlistPlant,
-          id: 1,
-        }
-      ];
+      const dbWishlist = [{ ...newWishlistPlant, id: 3 }];
 
       const response = await fakeRequest(app)
         .post('/api/wishlist')
@@ -285,15 +275,153 @@ describe('app routes', () => {
         .expect('Content-Type', /json/)
         .expect(200);
       expect(response.body).toEqual(dbWishlist);
-
-      // const getWishlistResponse = await fakeRequest(app)
-      //   .get('/api/wishlist')
-      //   .set('Authorization', token)
-      //   .expect('Content-Type', /json/);
-      //   // .expect(200);
-
     });
 
+    // GET our wishlist
+    test('gets the wishlist', async () => {
+      const dbWishlist = [
+        {
+          main_species_id: 122263,
+          owner_id: 2,
+          id: 3,
+        },
+      ];
 
+      const response = await fakeRequest(app)
+        .get('/api/wishlist')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(response.body).toEqual(dbWishlist);
+    });
+
+    // DELETE an item from our wishlist
+    test('deletes an item from the wishlist', async () => {
+      const expectation = [
+        {
+          main_species_id: 122263,
+          owner_id: 2,
+          id: 3,
+        },
+      ];
+
+      const response = await fakeRequest(app)
+        .delete('/api/wishlist/3')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(response.body).toEqual(expectation);
+
+      const nothing = await fakeRequest(app)
+        .get('/api/wishlist')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(nothing.body).toEqual([]);
+    });
+
+    // POST plant to my_garden
+    test('adds a plant to the my_garden', async () => {
+      const newPlant = {
+        main_species_id: 122263,
+        plant_name: 'Jordan',
+        owner_id: 2,
+      };
+
+      const dbPlantlist = [{ ...newPlant, id: 3 }];
+
+      const response = await fakeRequest(app)
+        .post('/api/my_garden')
+        .send(newPlant)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(response.body).toEqual(dbPlantlist);
+    });
+
+    // GET my_garden items
+    test('gets my_garden', async () => {
+      const dbMyGarden = [
+        {
+          main_species_id: 122263,
+          owner_id: 2,
+          plant_name: 'Jordan',
+          id: 3,
+        },
+      ];
+
+      const response = await fakeRequest(app)
+        .get('/api/my_garden')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(response.body).toEqual(dbMyGarden);
+    });
+
+    // POST to notes
+    test('adds a post to user notes', async () => {
+      const newNote = {
+        main_species_id: 122263,
+        owner_id: 2,
+        date: '2021-04-02',
+        note: 'Watered a lot',
+      };
+
+      const dbJournal = [{ ...newNote, id: 3 }];
+
+      const response = await fakeRequest(app)
+        .post('/api/notes')
+        .send(newNote)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(response.body).toEqual(dbJournal);
+    });
+
+    // GET notes
+    test('gets user notes for one plant', async () => {
+      const plant = [
+        {
+          main_species_id: 122263,
+          owner_id: 2,
+          date: '2021-04-02',
+          note: 'Watered a lot',
+          id: 3,
+        },
+      ];
+
+      const response = await fakeRequest(app)
+        .get('/api/notes/122263')
+        .set('Authorization', token);
+      // .expect('Content-Type', /json/);
+      // .expect(200);
+      expect(response).toEqual(plant);
+    });
+
+    // DELETE an item from my_garden
+    test('deletes an item from my_garden', async () => {
+      const expectation = [
+        {
+          main_species_id: 122263,
+          owner_id: 2,
+          plant_name: 'Jordan',
+          id: 3,
+        },
+      ];
+
+      const response = await fakeRequest(app)
+        .delete('/api/my_garden/3')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(response.body).toEqual(expectation);
+
+      const nothing = await fakeRequest(app)
+        .get('/api/my_garden')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(nothing.body).toEqual([]);
+    });
   });
 });
